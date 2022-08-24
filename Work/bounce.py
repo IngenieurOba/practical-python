7f2276ea3c62b4cb61fed90463acd5abeb116862
IngenieurOba <toba.kudehinbu@gmail.com>
2022-08-24 13:25:59 +0100

    Included bill_height file

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,16 @@
 # bounce.py
 #
 # Exercise 1.5
+
+initial_height = 100 # (in Meters)
+present_height = initial_height
+count = 0
+distance_reductor = 3/5
+
+
+while count < 10:
+    present_height = present_height * distance_reductor
+    print(count, end=" ")
+    print(round(present_height, 4))
+    count += 1
+
